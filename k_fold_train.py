659a22640b0ba9e5b340cdbb4e4baacbf169e67f
ShaoXiang23 <48667632+ShaoXiang23@users.noreply.github.com>
2019-10-26 14:56:09 +0800

    Update k_fold_train.py

--- k_fold_train.py
+++ k_fold_train.py
@@ -7,7 +7,10 @@ import torch.utils.model_zoo as model_zoo
 import torch.nn as nn
 import torchvision
 import numpy as np
+import copy
+import os
 from models.resnet import resnet50
+
 
 classes = ["中云-高积云-絮状高积云", "中云-高积云-透光高积云",
            "中云-高积云-荚状高积云", "中云-高积云-积云性高积云",
@@ -32,6 +35,12 @@ k4_path = ('datasets/Train_kf_4.csv', 'datasets/Test_kf_4.csv')
 k5_path = ('datasets/Train_kf_5.csv', 'datasets/Test_kf_5.csv')
 kfold_lists = [k1_path, k2_path, k3_path, k4_path, k5_path]
 
+# function parameters
+model_path = 'weights/resnet50'
+train_batch_size = 1
+verify_batch_size = 1
+
+
 # parameters
 k = 5
 epochs = 3
@@ -41,15 +50,29 @@ momentum = 0.9
 global_step = 0
 
 if __name__ == "__main__":
+
+    # record fold i the min_loss
+    loss_info = []
+
     # K-time training
     for fold in range(k):
         model = resnet50(pretrained=True, my_classes=29)
         if torch.cuda.is_available():
             model = model.cuda()
 
+        # set corresponding train_data and test_data
+        train_data = MyDataset(kfold_lists[fold][0])
+        train_loader = DataLoader(dataset=train_data, batch_size=train_batch_size, shuffle=True, num_workers=2, pin_memory=True)
+
+        # min_loss
         min_loss = [0.9, 0.9]
+
+        # training
         for epoch in range(epochs):
             print('---------- Epoch %d: training ---------' % epoch)
+            model.train()
+
+            # adjust learning rate by epoch
             if epoch == 0:
                 optimizer = torch.optim.Adam(model.parameters(), lr=lr, weight_decay=weight_decay)
             elif epoch == 1:
@@ -57,19 +80,19 @@ if __name__ == "__main__":
             else:
                 optimizer = torch.optim.Adam(model.parameters(), lr=lr/50, weight_decay=weight_decay)
 
-            train_data = MyDataset(kfold_lists[fold][0])
-            train_loader = DataLoader(dataset=train_data, batch_size=32, shuffle=True, num_workers=2, pin_memory=True)
-
-
             for step, (data, target) in enumerate(train_loader):
                 if torch.cuda.is_available():
                     data = data.cuda()
                     target = target.cuda()
+
                 optimizer.zero_grad()
                 output = model(data)
-                loss = torch.nn.CrossEntropyLoss(output, target)
+
+                # special loss: binary_cross_entroy
+                loss = torch.nn.BCELoss(output, target)
                 loss.backward()
                 optimizer.step()
+
                 global_step += 1
 
                 # step verify
@@ -77,6 +100,8 @@ if __name__ == "__main__":
                     model.eval()
                     if torch.cuda.is_available():
                         model = model.cuda()
+
+                    # corresponding verify datasets for kfold
                     verify_data = MyDataset(kfold_lists[fold][1])
                     verify_loader = DataLoader(dataset=verify_data, batch_size=32, shuffle=True, num_workers=2, pin_memory=True)
                     total_vloss = 0
@@ -86,24 +111,37 @@ if __name__ == "__main__":
                             vtarget = vtarget.cuda()
 
                         voutput = model(vdata)
-                        vloss = torch.nn.CrossEntropyLoss(voutput, vtarget)
+                        vloss = torch.nn.BCELoss(voutput, vtarget)
                         total_vloss += vloss.data[0]
 
-                    vloss = total_vloss / (10211 * 0.2 // 32)
+                    vloss = total_vloss / (10211 * 0.2 / verify_batch_size)
 
                     model.train()
                     if torch.cuda.is_available():
                         model = model.cuda()
 
-                    # print('{} Fold{} Epoch{} Step{}: [{}/{} ({:.0f}%)]\tTrain Loss: {:.6f}\tValidate Loss: {:.6f}'.format(
-                    #         model_path.split('/')[-1], fold, epoch, global_step, step * 32,
-                    #         len(train_loader.dataset),
-                    #         100. * step / len(train_loader),
-                    #         loss.data[0], vloss))
-                    
+                    print('{} Fold{} Epoch{} Step{}: [{}/{} ({:.0f}%)]\tTrain Loss: {:.6f}\tValidate Loss: {:.6f}'.format(
+                            model_path.split('/')[-1],
+                            fold,
+                            epoch,
+                            global_step,
+                            step * train_batch_size,
+                            len(train_loader.dataset),
+                            100. * step / len(train_loader),
+                            loss.data[0],
+                            vloss))
+
                     if vloss < min_loss[1]:
                         min_loss[1] = vloss
                         min_loss[0] = loss.data[0]
-    
 
-        model_save = copy.
+
+        # save model
+        model_save = copy.deepcopy(model)
+        torch.save(model_save.cpu(), os.path.join(model_path, 'resnet50_fold_%d.model' % (fold)))
+        loss_info.append(min_loss)
+
+    print('-' * 20)
+    print(model_path.split('/')[-1] + ":")
+    for i, l in enumerate(loss_info):
+        print("Fold %d: Train loss:%f\tVerify loss:%f" % (i, l[0], l[1]))
